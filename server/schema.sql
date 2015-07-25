c64912e844aff6621befa92505abf0dc303fd897
marineAndrea <marine.guillanton@gmail.com>
2015-07-24 17:31:47 -0700

    Update schema with users table

--- server/schema.sql
+++ server/schema.sql
@@ -3,12 +3,19 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  ID int AUTO_INCREMENT PRIMARY KEY,
+  userID int,
+  messageBody varchar(142),
+  room varchar(64),
+  createdAt char(24) NOT NULL
 );
 
 /* Create other tables and define schemas for them here! */
 
-
+CREATE TABLE users (
+  ID int AUTO_INCREMENT PRIMARY KEY,
+  name varchar(64)
+);
 
 
 /*  Execute this file from the command line by typing:
--- server/spec/server-spec.js
+++ server/spec/server-spec.js
@@ -16,7 +16,7 @@ describe("Persistent Node Chat Server", function() {
     });
     dbConnection.connect();
 
-       var tablename = ""; // TODO: fill this out
+       var tablename = "messages"; // TODO: fill this out
 
     /* Empty the db table before each test so that multiple tests
      * (or repeated runs of the tests) won't screw each other up: */
@@ -47,7 +47,7 @@ describe("Persistent Node Chat Server", function() {
 
         // TODO: You might have to change this test to get all the data from
         // your message table, since this is schema-dependent.
-        var queryString = "SELECT * FROM messages";
+        var queryString = "SELECT messages.ID, users.name, messages.messageBody, messages.room, messages.createdAt FROM messages, users WHERE messages.userID = users.ID;";
         var queryArgs = [];
 
         dbConnection.query(queryString, queryArgs, function(err, results) {
@@ -55,7 +55,7 @@ describe("Persistent Node Chat Server", function() {
           expect(results.length).to.equal(1);
 
           // TODO: If you don't have a column named text, change this test.
-          expect(results[0].text).to.equal("In mercy's name, three days is all I need.");
+          expect(results[0].messageBody).to.equal("In mercy's name, three days is all I need.");
 
           done();
         });
@@ -65,7 +65,12 @@ describe("Persistent Node Chat Server", function() {
 
   it("Should output all messages from the DB", function(done) {
     // Let's insert a message into the db
-       var queryString = "";
+       var queryString =  "insert into messages \
+                            (userID, messageBody, room, createdAt) \
+                            values ('0', 'Men like you can never change!', 'main', '2015-07-24T22:07:50.521Z'); \
+                          insert into users \
+                            (name) \
+                            values ('Brigitte');";
        var queryArgs = [];
     // TODO - The exact query string and query args to use
     // here depend on the schema you design, so I'll leave
@@ -78,8 +83,8 @@ describe("Persistent Node Chat Server", function() {
       // the message we just inserted:
       request("http://127.0.0.1:3000/classes/messages", function(error, response, body) {
         var messageLog = JSON.parse(body);
-        expect(messageLog[0].text).to.equal("Men like you can never change!");
-        expect(messageLog[0].roomname).to.equal("main");
+        expect(messageLog[0].messageBody).to.equal("Men like you can never change!");
+        expect(messageLog[0].room).to.equal("main");
         done();
       });
     });
